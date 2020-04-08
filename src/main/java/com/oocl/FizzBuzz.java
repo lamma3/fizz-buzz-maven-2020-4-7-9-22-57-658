ce810a7313a7743f6dd3492fcad4ee8bff1b5344
Marco Lam <lamma3@oocl.com>
2020-04-08 15:59:28 +0800

    [Marco] pass test case test_say_when_receive3_then_returnFizz

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -1,8 +1,17 @@
 package com.oocl;
 
 public class FizzBuzz {
+    
+    private final static String FIZZ = "Fizz";
 
     public String say(int number) {
+        if (isDivisibleBy3(number)) {
+            return FIZZ;
+        }
         return String.valueOf(number);
+    }
+
+    private boolean isDivisibleBy3(int number) {
+        return number % 3 == 0;
     }
 }
