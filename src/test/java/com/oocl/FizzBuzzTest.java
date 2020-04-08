330b64f0ed4797a455871b2e7fa02e054b54b312
Marco Lam <lamma3@oocl.com>
2020-04-08 15:56:14 +0800

    [Marco] pass test case test_say_when_receive1_then_return1

--- src/test/java/com/oocl/FizzBuzzTest.java
+++ src/test/java/com/oocl/FizzBuzzTest.java
@@ -8,7 +8,7 @@ public class FizzBuzzTest {
     @Test
     public void test_say_when_receive1_then_return1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String result = fizzBuzz.say("1");
+        String result = fizzBuzz.say(1);
         Assert.assertEquals("1", result);
     }
 }
