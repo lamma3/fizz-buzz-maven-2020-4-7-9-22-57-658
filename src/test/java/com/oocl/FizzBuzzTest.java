15f926f660f22f671ba2ae8d51fc0791798dac59
Marco Lam <lamma3@oocl.com>
2020-04-08 15:54:41 +0800

    [Marco] add test case test_say_when_receive1_then_return1

---
+++ src/test/java/com/oocl/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.oocl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void test_say_when_receive1_then_return1() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.say("1");
+        Assert.assertEquals("1", result);
+    }
+}
